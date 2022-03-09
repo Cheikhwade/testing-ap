68a92c4c79b030c1c3ff2258b61d255abf57f021
Cheikh <cheikhahmadouwade@gmail.com>
2022-03-09 16:24:05 +0000

    mes dossier marche

--- public/js/app.js
+++ public/js/app.js
@@ -21304,8 +21304,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\wamp\www\laravel8crud\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\wamp\www\laravel8crud\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! /home/serigne/Bureau/Teste-App/testing/laravel8logindashcrud/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/serigne/Bureau/Teste-App/testing/laravel8logindashcrud/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
